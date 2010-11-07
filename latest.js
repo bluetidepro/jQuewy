2bcaddbb28c4ae3a68a0857cb86c3ecfc67d16be
John Hamelink <s0l1dsnak3123@gmail.com>
2010-11-07 16:08:54 +0000

    Added stylesheet support, Added a framework for ajax

--- latest.js
+++ latest.js
@@ -1,4 +1,31 @@
-/* jQuewy 0.4 */
+/*
+ * Copyright (c) 2010 jQuewy Project
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * jQuewy v0.4
+ * 
+ */
+ 
 var jsonfile = 'libs.php';
 var data = null;
 function i(p){ 
@@ -36,9 +63,12 @@ function init(time_elapsed) {
 function j(s){
 	if(s){
 		var data = init(0);
-		//multiple libraries support
+		//multiple libraries support:
+		//if it's an array:
 		if (s instanceof Array){
+			//For each library asked for
 			for (var n in s){
+				
 				//For each available library
 				for (var key in data) {
 					var obj = data[key];
@@ -48,6 +78,7 @@ function j(s){
 						i(url);
 					}
 				}
+				
 			}
 		}
 		//For each available library
@@ -59,8 +90,7 @@ function j(s){
 				i(url);
 			}
 		}
-	}
-	else {
+	} else {
 		return "0.4";
 	}
 }
