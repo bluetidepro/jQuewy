3441c3a2de1e21ecdd262279be04e0d3d9e55280
John Hamelink <s0l1dsnak3123@gmail.com>
2010-11-08 17:11:27 +0000

    Added prototype, scrollto, mootools, dojo. scriptaculous, swfobject, chromeframe, gritter, tipsy and UI. Minified the scripts on the amazon S3 storage.

--- libs.php
+++ libs.php
@@ -11,7 +11,7 @@
 		"easing":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.easing.1.3.js"}],
 		"backbone":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/backbone-min.js"}],
 		"alphanumeric":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.alphanumeric.pack.js"}],
-		"prototype":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/prototype.js"}],
+		"prototype":[{"url": "http://ajax.googleapis.com/ajax/libs/prototype/1.6.1.0/prototype.js"}],
 		"tools":[{"url": "http://cdn.jquerytools.org/1.2.5/jquery.tools.min.js"}],
 		"flot":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.flot.js"}],
 		"html5":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/html5.js"}],
@@ -21,13 +21,23 @@
 		"hashchange":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.ba-hashchange.min.js"}],
 		"simpleautogrow":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.simpleautogrow.js"}],
 		"simplemodal":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.simplemodal-1.3.5.min.js"}],
+		"scrollto":[{"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.scrollTo-min.js"}],
+		"mootools":[{"url": "http://ajax.googleapis.com/ajax/libs/mootools/1.3.0/mootools-yui-compressed.js"}],
+		"dojo":[{"url": "http://ajax.googleapis.com/ajax/libs/dojo/1.5/dojo/dojo.xd.js"}],
+		"scriptaculous":[{"url": "http://ajax.googleapis.com/ajax/libs/scriptaculous/1.8.3/scriptaculous.js"}],
+		"swfobject":[{"url": "http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"}],
+		"chromeframe":[{"url": "http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"}],
 		"ui":[{
-			"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.ui.js",
+			"url": "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js",
 			"stylesheet": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.ui.style.css"
 		}],
 		"gritter":[{
 			"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.gritter.min.js",
 			"stylesheet": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.gritter.css"
-		}]
+		}],
+		"tipsy":[{
+			"url": "https://s3-eu-west-1.amazonaws.com/jquewyeu/jquery.tipsy.js",
+			"stylesheet": "https://s3-eu-west-1.amazonaws.com/jquewyeu/tipsy.css"
+		}
 	}
 });
