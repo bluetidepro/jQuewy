14557f0ac90a2db7d1cc0490eb8dd7dbad8694b6
Becojo <benoit.cote.jodoin@gmail.com>
2010-11-07 20:11:55 -0500

    recoded the core

--- latest.js
+++ latest.js
@@ -1,83 +1,64 @@
-var jsonfile = 'http://jquewy.com/libs.php';
-var data = null;
+(function(){
 
-
-function i(p){ 
-	var i, src = p, scripts = document.getElementsByTagName("script"); 
-	for (i=0; i<scripts.length; i++){if (scripts[i].src.match(src)){ base = scripts[i].src.replace(src, "");break;}} 
-	document.write("<" + "script src=\"" + p + "\"></" + "script>"); 
-}
-
-function style(p){
-	var i, href = p, styles = document.getElementsByTagName("link"); 
-	for (i=0; i<styles.length; i++){if (styles[i].href.match(href)){ base = styles[i].href.replace(href, "");break;}} 
-	document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + p + "\" />"); 
-}
-
-function jsonp(url,callback,name, query){
-	if (url.indexOf("?") > -1){
-		url += "&callback=" 
-	} else {
-		url += "?callback="
-	}
-	url += name + "&";
-	if (query){
-		url += encodeURIComponent(query) + "&";
-	}
-	url += new Date().getTime().toString(); // prevent caching        
-
-	var script = document.createElement("script");
-	script.setAttribute("src",url);
-	script.setAttribute("type","text/javascript");
-	document.head.appendChild(script);
-}
-
-function parse(s){
-	data = s.libs;
-	return s;
-}
-
-jsonp(jsonfile,1,'parse');
-
-i("https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js");
-
-function $j(s){
+	var jQuewy = function(libs,callback){
+		for(var i = 0, length = libs.length; i < length; i++){
+			var lib = jQuewy.data[libs[i].toLowerCase()];
+			if(lib){
+				jQuewy.addScript(lib[0].url);
+				if(lib[0].stylesheet) jQuewy.addStylesheet(lib[0].stylesheet);
+			}else throw 'jQuewy does not support ' + libs[i];
+		}
+		jQuewy.addEvent(window,'load',callback);
+	};
 	
-	if (s instanceof Array){
-		for (var n in s){
-			//For each available library
+	jQuewy.extend = function(destination, source) { // Taken from Prototype
+		for (var property in source) destination[property] = source[property];
+		return destination;
+	}
+
+	jQuewy.extend(jQuewy, {
+		libFile: "http://jquewy.com/libs.php",
+		data: false,
+		
+		addScript: function(src){
+			document.write('<'+'script type="text/javascript" src="' + src +'">'+'</'+'script>');	
+		},
+		
+		addStylesheet: function(href){
+			document.write('<link rel="stylesheet" type="text/css" href="' + href + '" />');
+		},
+		
+		jsonp: function(url, name, query){
+			url += (url.indexOf("?") > -1) ? "&callback="  : "?callback="
+			url += name + "&";
+			if (query) url += encodeURIComponent(query) + "&";
+			url += new Date().getTime().toString(); // prevent caching   
+			jQuewy.addScript(url);
+		},
+		
+		parse: function(data){
+			jQuewy.data = data.libs;
+		},
+		
+		list: function(){
+			var return_string = "Available Libraries:\n",
+			    data = jQuewy.data;
 			for (var key in data) {
-				var obj = data[key];
 				var name = key;
-				var url = obj[0]['url'];
-				if (s[n] == name){
-					i(url);
-					if (obj[0]['stylesheet'] !== undefined){
-						style(obj[0]['stylesheet']);
-					}
-				}
+				var url = data[key][0]['url'];
+				return_string += name + ",\n";
 			}
+			alert(return_string);
+		},
+		
+		addEvent: function(element, type, callback){
+			if(element.addEventListener) element.addEventListener(type, callback, false);
+			else if(element.attachEvent) element.attachEvent('on'+element, callback);
 		}
-	}
-	//For each available library
-	for (var key in data) {
-		var obj = data[key];
-		var name = key;
-		var url = obj[0]['url'];
-		if (s == name){
-			i(url);
-		}
-	}
+	});
 	
-}
-
-function $list(){
-	var return_string = "Available Libraries:\n";
-	for (var key in data) {
-		var name = key;
-		var url = data[key][0]['url'];
-		return_string += name + ",\n";
-	}
-	alert(return_string);
-}
-
+	jQuewy.jsonp(jQuewy.libFile,'jQuewy.parse');
+	
+	window.jQuewy = window.$j = jQuewy;
+	
+})();
